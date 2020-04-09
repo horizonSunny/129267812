458609cbf45f7abc09a565fb91c8ecffa3bf1e31
sunyu1 <sunyu1@green-valley.com>
2020-04-09 12:18:35 +0800

    add

--- src/pages/businessAdm/particularsComponents/index.tsx
+++ src/pages/businessAdm/particularsComponents/index.tsx
@@ -1,6 +1,3 @@
 import Title from './Title';
-import BaseInfo from './BaseInfo';
-import Qualification from './Qualification';
-import Shopscene from './Shopscene';
-import Operationrecord from './Operationrecord';
-export { Title, BaseInfo, Qualification, Shopscene, Operationrecord };
+
+export { Title };
