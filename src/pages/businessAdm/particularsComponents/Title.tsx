815eb6a0eb519300112e013cb0277c3588f1cb27
sunyu1 <sunyu1@green-valley.com>
2020-04-09 12:11:25 +0800

    add

--- src/pages/businessAdm/particularsComponents/Title.tsx
+++ src/pages/businessAdm/particularsComponents/Title.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'dva';
 import styles from './index.less';
 import { orderStatus } from '@/utils/configInfo';
+import tipsIcon from '@/assets/order/Tips-icon.svg';
 
 @connect(({ businessAdm }) => ({
   businessAdm,
@@ -15,7 +16,10 @@ class Title extends Component {
           <span>{orderStatus(currentRecord.orderStatus)}</span>
           <span className={`${styles.timeRemaining}`}>剩余23小时56分59秒自动关闭</span>
         </div>
-        <div>1</div>
+        <div className={`${styles.expressage}`}>
+          <img src={tipsIcon} alt="123" />
+          <span>普通快递</span>
+        </div>
       </div>
     );
   }
