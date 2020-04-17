a266cfe2daf3de83b8f6ced884e2b481dca1d538
sunyu1 <sunyu1@green-valley.com>
2020-04-17 15:43:22 +0800

    add

--- src/pages/tradeSetting/noDistribute/noDistribute.tsx
+++ src/pages/tradeSetting/noDistribute/noDistribute.tsx
@@ -6,7 +6,34 @@ import mapImg from '@/assets/tradeSetting/map-da.svg';
 // 外部引入
 import AreaSelect from '../components/areaSelect';
 import styles from './noDistribute.less';
+import { filterAreaName } from '@/utils/filterProperty';
 
+function processingArea(area) {
+  // 首先过滤先把所有的省拿到，这些省都是全选的
+  const result = {};
+  const city = [];
+  // const selectPartProvince = [];
+  area.map(item => {
+    if (item.length === 2) {
+      // selectAllProvince.push(item);
+      result[item] = 'all';
+    } else {
+      city.push(item);
+    }
+  });
+  // city
+  city.map(item => {
+    // 省份id是唯一的，可以用作key值
+    const provinceId = item.slice(0, 2);
+    if (result.hasOwnProperty(provinceId)) {
+      result[provinceId].push(item);
+    } else {
+      result[provinceId] = [];
+      result[provinceId].push(item);
+    }
+  });
+  return result;
+}
 @connect(({ nonDelivery }) => ({
   nonDelivery,
 }))
@@ -34,11 +61,13 @@ export default class NoDistribute extends React.Component {
 
   render() {
     const { nonDeliveryInfo } = this.props.nonDelivery;
-    console.log('nonDeliveryInfo_', nonDeliveryInfo);
-
+    // const {keys,values}
+    const area = processingArea(nonDeliveryInfo.deliveryArea);
+    // const area = processingArea(['11', '32', '6531', '6532', '1301', '1302']);
+    console.log('area_', area);
     return (
       <PageHeaderWrapper>
-        {nonDeliveryInfo && (
+        {!nonDeliveryInfo && (
           <div className={`${styles.noDistribute}`}>
             <img src={mapImg} alt="" />
             <span>你还未配置不可配送区域</span>
@@ -47,40 +76,32 @@ export default class NoDistribute extends React.Component {
             </Button>
           </div>
         )}
-        {!nonDeliveryInfo && (
+        {nonDeliveryInfo && (
           <div className={`${styles.main}`}>
             <Row className={styles.selfDelivery}>
               <Col span={20} className={styles.selfDeliveryInfo}>
                 <table className={`${styles.tableInfo}`}>
-                  <tr>
-                    <td>门店自提点</td>
-                    <td>安康大药房</td>
-                    {/* <td>{selfDeliveryInfo.tenantName}</td> */}
-                  </tr>
-                  <tr>
-                    <td> 地址</td>
-                    <td> 上海市 浦东新区海科路100号</td>
-                    {/* <td>{selfDeliveryInfo.address}</td> */}
-                  </tr>
-                  <tr>
-                    <td> 联系电话</td>
-                    <td> 021-89679867</td>
-                    {/* <td>{selfDeliveryInfo.adminTel}</td> */}
-                  </tr>
-                  <tr>
-                    <td> 联系电话</td>
-                    <td> 021-89679867</td>
-                    {/* <td>{selfDeliveryInfo.adminTel}</td> */}
-                  </tr>
-                  <tr>
-                    <td> 营业时间</td>
-                    <td> 周一/周三/周五 08:30-20:30</td>
-                  </tr>
+                  {/* <tr>
+                    <td>省</td>
+                    <td>市</td>
+                  </tr> */}
+                  {Object.keys(area).map(item => {
+                    // const province = filterAreaName(item)
+                    return (
+                      <tr>
+                        <td>{filterAreaName(item)}</td>
+                        <td>
+                          {area[item] !== 'all' && filterAreaName(area[item])}
+                          {area[item] === 'all' && '全部'}
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </table>
               </Col>
               <Col span={1}>
-                <Button className={`${styles.editorButton}`} onClick={this.toSelfDelivery}>
-                  编辑
+                <Button className={`${styles.editorButton}`} onClick={this.showArea}>
+                  配置
                 </Button>
               </Col>
             </Row>
