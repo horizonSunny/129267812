f5605c455eb08f5a933831263242ec2c2967061e
sunyu1 <sunyu1@green-valley.com>
2020-04-17 14:53:52 +0800

    add

--- src/pages/tradeSetting/noDistribute/noDistribute.tsx
+++ src/pages/tradeSetting/noDistribute/noDistribute.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
-import { Row, Col, Button, Tree } from 'antd';
+import { Row, Col, Button } from 'antd';
 import { connect } from 'dva';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import mapImg from '@/assets/tradeSetting/map-da.svg';
-import { newAreaTree } from '@/utils/area.js';
 // 外部引入
 import AreaSelect from '../components/areaSelect';
 import styles from './noDistribute.less';
 
-const options = newAreaTree();
-const { TreeNode } = Tree;
-// const { Search } = Input;
-// @connect(({ commodityClassify }) => ({
-//   commodityClassify,
-// }))
+@connect(({ nonDelivery }) => ({
+  nonDelivery,
+}))
 export default class NoDistribute extends React.Component {
   componentDidMount() {
-    console.log('areaInfo_', options);
     const { dispatch } = this.props;
-    // if (dispatch) {
-    //   dispatch({
-    //     type: 'commodityClassify/classification',
-    //   }).then(() => {
-    //     // 查询单个分类的商品
-    //     dispatch({
-    //       type: 'commodityClassify/selectCas',
-    //       payload: this.props.commodityClassify.casInfoOne[0],
-    //     });
-    //   });
-    // }
+    if (dispatch) {
+      dispatch({
+        type: 'nonDelivery/getNonDelivery',
+      });
+    }
   }
 
   // 调用子组件方法
@@ -44,15 +33,59 @@ export default class NoDistribute extends React.Component {
   };
 
   render() {
+    const { nonDeliveryInfo } = this.props.nonDelivery;
+    console.log('nonDeliveryInfo_', nonDeliveryInfo);
+
     return (
       <PageHeaderWrapper>
-        <div className={`${styles.main}`}>
-          <img src={mapImg} alt="" />
-          <span>你还未配置不可配送区域</span>
-          <Button type="primary" className={`${styles.button}`} onClick={this.showArea}>
-            去配置 &gt;
-          </Button>
-        </div>
+        {nonDeliveryInfo && (
+          <div className={`${styles.noDistribute}`}>
+            <img src={mapImg} alt="" />
+            <span>你还未配置不可配送区域</span>
+            <Button type="primary" className={`${styles.button}`} onClick={this.showArea}>
+              去配置 &gt;
+            </Button>
+          </div>
+        )}
+        {!nonDeliveryInfo && (
+          <div className={`${styles.main}`}>
+            <Row className={styles.selfDelivery}>
+              <Col span={20} className={styles.selfDeliveryInfo}>
+                <table className={`${styles.tableInfo}`}>
+                  <tr>
+                    <td>门店自提点</td>
+                    <td>安康大药房</td>
+                    {/* <td>{selfDeliveryInfo.tenantName}</td> */}
+                  </tr>
+                  <tr>
+                    <td> 地址</td>
+                    <td> 上海市 浦东新区海科路100号</td>
+                    {/* <td>{selfDeliveryInfo.address}</td> */}
+                  </tr>
+                  <tr>
+                    <td> 联系电话</td>
+                    <td> 021-89679867</td>
+                    {/* <td>{selfDeliveryInfo.adminTel}</td> */}
+                  </tr>
+                  <tr>
+                    <td> 联系电话</td>
+                    <td> 021-89679867</td>
+                    {/* <td>{selfDeliveryInfo.adminTel}</td> */}
+                  </tr>
+                  <tr>
+                    <td> 营业时间</td>
+                    <td> 周一/周三/周五 08:30-20:30</td>
+                  </tr>
+                </table>
+              </Col>
+              <Col span={1}>
+                <Button className={`${styles.editorButton}`} onClick={this.toSelfDelivery}>
+                  编辑
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        )}
         <AreaSelect onRef={this.onRefInfo} />
       </PageHeaderWrapper>
     );
--- src/services/tradeSetting.ts
+++ src/services/tradeSetting.ts
@@ -6,6 +6,10 @@ export async function getPickUp(params: Object) {
     params,
   });
 }
+// 获取不可配送区域
+export async function getNonDelivery() {
+  return request('/admin/v1/tenant/getNonDelivery');
+}
 
 // 获取运费模版列表数据
 export async function freightList(params: Object) {
@@ -33,38 +37,3 @@ export async function deleteFreight(params: Object) {
     method: 'delete',
   });
 }
-// // 快速分类顺序调整
-// export async function reverseCategoryList(params: Object) {
-//   return request('/admin/v1/quickCategory/update/order', {
-//     method: 'put',
-//     data: params,
-//   });
-// }
-// // 删除快速分类
-// export async function deleteCategoryItem(params: Object) {
-//   return request(`/admin/v1/quickCategory/delete/${params.quickCategoryId}`, {
-//     method: 'delete',
-//   });
-// }
-// // 新增快速分类接口
-// export async function newCategoryItem(params: Object) {
-//   return request('/admin/v1/quickCategory/insert', {
-//     method: 'post',
-//     data: params,
-//   });
-// }
-// // 编辑快速分类接口
-// export async function editorCategoryItem(params: Object) {
-//   return request('/admin/v1/quickCategory/update', {
-//     method: 'put',
-//     data: params,
-//   });
-// }
-
-// // 改变快速分类状态
-// export async function changeCategoryItem(params: Object) {
-//   return request('/admin/v1/quickCategory/update/status', {
-//     method: 'put',
-//     data: params,
-//   });
-// }
