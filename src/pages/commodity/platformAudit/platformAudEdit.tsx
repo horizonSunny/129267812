6dc53066ae4809bc266628f4e6dfbcd0d2fcd610
sunyu1 <sunyu1@green-valley.com>
2020-06-09 14:27:23 +0800

    add

--- src/pages/commodity/platformAudit/platformAudEdit.tsx
+++ src/pages/commodity/platformAudit/platformAudEdit.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Card, Typography, Alert, Button } from 'antd';
 import { connect } from 'dva';
-import styles from './platformAudEdit.less';
+import router from 'umi/router';
+import styles from './platformAudDet.less';
+import Product from './component/platformCommon/product';
+import Edit from './component/platformAudEdit/edit';
 
-@connect(({ platformAudit, tradeSetting }) => ({ platformAudit, tradeSetting }))
-export default class PlatformAudEdit extends React.Component {
-  state = {
-    isFirstForm: true,
-  };
+const { Title } = Typography;
 
-  componentDidMount() {
-    // getFreightList;
-    // const { dispatch } = this.props;
+@connect(({ platformAudit }) => ({ platformAudit }))
+export default class PlatformAudDet extends React.Component {
+  handleBack() {
+    router.goBack();
   }
 
   render() {
-    const { isFirstForm } = this.state;
-    return <PageHeaderWrapper className={styles.main} />;
+    const { productWithId } = this.props.platformAudit;
+    // const platformAudit = this.props.platformAudit.product.productSku;
+    return (
+      <PageHeaderWrapper className={styles.main}>
+        <Product />
+        <Edit />
+      </PageHeaderWrapper>
+    );
   }
 }
